f28362423ed77bb37547cf2cd6c574df1ea29831
LATIsi <p.kmj9911@gmail.com>
2022-03-11 09:54:41 +0000

    03.11 ES6 함수리터럴, 화살표함수

--- htmljs_s0301/test.js
+++ htmljs_s0301/test.js
@@ -1,2 +1,24 @@
-var test = document.getElementById("body").innerText;
-console.log(test);
+// var test = document.getElementById("body").innerText;
+// // console.log(test);
+
+// var test = document.getElementByTagName("input");
+
+// function getvalues () {
+// 	var value = document.getElementsByTagName("input")[0].value;
+// 	alert("당신이 입력한 아이디는"+value+"입니다");
+// }
+ const apa1=['가','나','다','라'];
+console.log(`원래 배열= ${apa1}`);
+
+ const apa2 = apa1.concat('아','자','차');
+ console.log(`concat = ${apa2}`);
+
+ apa1.push('마','바','사');
+ console.log(`push = ${apa1}`);
+
+
+
+const number=['가','나','다','라'];
+
+number.forEach( name => console.log(`내 이름은 ${name} 이야! `));
+
